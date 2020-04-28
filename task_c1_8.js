5dde9c1f61e3f0271e11d05ec38321ef8a95cb66
zpa1986 <59637710+zpa1986@users.noreply.github.com>
2020-04-28 11:45:17 +0700

    Update task_c1_8.js

--- task_c1_8.js
+++ task_c1_8.js
@@ -1,1 +1,23 @@
+jQuery.prototype.html = function (html) {
+if (html) {
+this.each(element=> element.innerHTML = html)
+return this.innerHTML
+}
+else {
+this.each(element=>element.innerHTML)
+return this.innerHTML
+}
+}
+
+
+jQuery.prototype.text = function (text) {
+if (text) {
+this.each(element=> element.innerText = text)
+return this.innerText
+}
+else {
+this.each(element=>element.innerText)
+return this.innerText
+}
+}
 
